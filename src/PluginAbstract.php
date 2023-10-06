6b9aa3d4ddec3ba47e76e26626949962118e0d76
nvanhaezebrouck <nicolas@oxygenz.fr>
2023-10-06 14:08:43 +0200

    init

---
+++ src/PluginAbstract.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace OxygenzSAS\Abstract;
+
+use OxygenzSAS\Interfaces\PluginInterface;
+
+abstract class PluginAbstract implements PluginInterface
+{
+
+    protected abstract static function getAllRequiredPlugin():array ;
+    protected abstract static function getCSS():array ;
+    protected abstract static function getJS():array ;
+    protected abstract static function getVIEW():array ;
+
+    public static function getPathView():array {
+        $r = static::getVIEW();
+        foreach (static::getAllRequiredPlugin() as $plugin){
+            $r = array_merge($plugin::getPathView(), $r);
+        }
+        return $r;
+    }
+
+    public static function getPathJS():array {
+        $r = static::getJS();
+        foreach (static::getAllRequiredPlugin() as $plugin){
+            $r = array_merge($plugin::getPathJS(), $r);
+        }
+        return $r;
+    }
+
+    public static function getPathCSS():array {
+        $r = static::getCSS();
+        foreach (static::getAllRequiredPlugin() as $plugin){
+            $r = array_merge($plugin::getPathCSS(), $r);
+        }
+        return $r;
+    }
+
+}
